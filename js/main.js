8214798447248d77d48a9e4faeb6be06a8f0455a
Frison Alexander <fri.b.alex@gmail.com>
2013-07-03 16:18:06 -0400

    working angular and JQM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+function FormController($scope){
 
+	$scope.MFData 				= {};
+	$scope.MFData.inspectorName = "From scope";
+	$scope.MFData.area 			= "Gas Inspection";
+	$scope.MFData.from 			= "NO Date";
+	$scope.MFData.to 			= "NO Date";
+
+
+	$scope.saveStateToLocal = function(){ localStorage["MFData"] = JSON.stringify($scope.MFData);}
+
+};
