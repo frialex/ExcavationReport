e4e6abffb685746b01f15adc30895dfc703130d1
Frison Alexander <fri.b.alex@gmail.com>
2013-07-18 13:52:13 -0400

    working on creating form input

--- js/main.js
+++ js/main.js
@@ -5,8 +5,24 @@ function FormController($scope){
 	$scope.MFData.area 			= "Gas Inspection";
 	$scope.MFData.from 			= "NO Date";
 	$scope.MFData.to 			= "NO Date";
+	$scope.MFData.workList 		= [];
 
 
 	$scope.saveStateToLocal = function(){ localStorage["MFData"] = JSON.stringify($scope.MFData);}
 
+	$scope.$on('AddNewWorkOrder', function(wo){
+		console.log("Received save signal for: " + wo);
+		$scope.MFData.workList.push(wo);
+	});
+
 };
+
+
+function InputController($scope){
+	$scope.submission = {}
+
+	$scope.saveWorkOrder = function(){
+		$scope.$emit('AddNewWorkOrder', $scope.submission);
+		console.log("Emitting save signal for " + $scope.submission);
+		}
+}
