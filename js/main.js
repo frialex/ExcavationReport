23d700a296589b96f1ad4f23c52d9eccc334d348
Frison Alexander <fri.b.alex@gmail.com>
2013-07-26 16:03:55 -0400

    Improved UI, and angular
    Able to edit, delete and save models.
    Angular bindings have been cleaned up.
    Removed second controller, as it added unnecessary complexity

--- js/main.js
+++ js/main.js
@@ -3,25 +3,36 @@ function FormController($scope){
 	//If nothing in local storage, initialize empty array
 	$scope.MFData = JSON.parse( localStorage["MFData"]  || '{"workList": []}' );
 
-
+	
 	$scope.saveStateToLocal = function(){ 
 		localStorage["MFData"] = JSON.stringify($scope.MFData);
 		console.log("Saved to local storage");
 	};
 
-	$scope.$on('AddNewWorkOrder', function(e, wo){
-		console.log("Received save signal");
-		$scope.MFData.workList.push(wo);
-	});
+	$scope.syncToServer = function(){
+		console.log("Start syncing data to server");
 
-};
+		//Save to local storage first
+		$scope.saveStateToLocal();
+
+		//If ther is internet connection, save to db
+	};
+
+	$scope.editModel = function(m){
+		$scope.model = m;
+	};
+
+	$scope.addModel = function(){
+		$scope.model = {};
+		$scope.MFData.workList.push($scope.model);
+	};
+
+	$scope.deleteWorkOrder = function(wo){
+		var index = $scope.MFData.workList.indexOf(wo);
+		console.log("Deleting index: " + index);
+		$scope.MFData.workList.splice(index,1);
+	};
 
 
-function InputController($scope){
-	$scope.submission = {}
-
-	$scope.saveWorkOrder = function(){
-		console.log("Emitting save signal");
-		$scope.$emit('AddNewWorkOrder', $scope.submission);
-		};
 }
+
